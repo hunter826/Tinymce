hunter826/Tinymce

commit 8a0136c0ed9902da862917c891fe9c2ec22ba9ef

Fixed bug where IE 9 wouldn't be able to select text after an editor instance with caret focus was removed.

js/tinymce/classes/Editor.js
@@ -2017,21 +2017,26 @@ define("tinymce/Editor", [
 			var self = this;
 
 			if (!self.removed) {
+				self.removed = 1;
 				self.save();
-				self.fire('remove');
-				self.off();
-				self.removed = 1; // Cancels post remove event execution
 
 				// Remove any hidden input
 				if (self.hasHiddenInput) {
 					DOM.remove(self.getElement().nextSibling);
 				}
 
-				// Don't clear the window or document if content editable
-				// is enabled since other instances might still be present
 				if (!self.inline) {
+					// IE 9 has a bug where the selection stops working if you place the
+					// caret inside the editor then remove the iframe
+					if (ie && ie < 10) {
+						self.getDoc().execCommand('SelectAll', false, null);
+					}
+
 					DOM.setStyle(self.id, 'display', self.orgDisplay);
 					self.getBody().onload = null; // Prevent #6816
+
+					// Don't clear the window or document if content editable
+					// is enabled since other instances might still be present
 					Event.unbind(self.getWin());
 					Event.unbind(self.getDoc());
 				}
@@ -2039,6 +2044,8 @@ define("tinymce/Editor", [
 				var elm = self.getContainer();
 				Event.unbind(self.getBody());
 				Event.unbind(elm);
+
+				self.fire('remove');
 
 				self.editorManager.remove(self);
 				DOM.remove(elm);

js/tinymce/classes/util/Observable.js
@@ -45,13 +45,14 @@ define("tinymce/util/Observable", [
 		fire: function(name, args, bubble) {
 			var self = this, handlers, i, l, callback, parent;
 
-			if (self.removed) {
-				return;
-			}
-
 			name = name.toLowerCase();
 			args = args || {};
 			args.type = name;
+
+			// Prevent all events except the remove event after the editor has been removed
+			if (self.removed && name !== "remove") {
+				return;
+			}
 
 			// Setup target is there isn't one
 			if (!args.target) {

commit 77fed6b7663f8bee8ce54cbf1e447ecfd8800c5a

Fix bug to fire postprocess and other save events right before removal/unloading
Fixes issue possibly introduced in 8a0136c0ed where editor.removed is set to true
before it runs save().  This blocks postprocess and other events from firing
properly during the save.

For instance, the default content '<p>&nbsp;</p>' would remain after running
tinymce.remove() instead of being cleaned up.

js/tinymce/classes/Editor.js
@@ -2041,8 +2041,8 @@ define("tinymce/Editor", [
 			var self = this;
 
 			if (!self.removed) {
-				self.removed = 1;
 				self.save();
+				self.removed = 1;
 
 				// Remove any hidden input
 				if (self.hasHiddenInput) {